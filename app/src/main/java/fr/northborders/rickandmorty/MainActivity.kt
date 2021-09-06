851584d038aa98dce7454c2fb52bdf952a263577
Thibault Guegan <thibault@travelloapp.com>
2021-09-06 17:07:03 +1000

    Navigation component setup

--- app/src/main/java/fr/northborders/rickandmorty/MainActivity.kt
+++ app/src/main/java/fr/northborders/rickandmorty/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.support.HasSupportFragmentInjector
 import fr.northborders.rickandmorty.databinding.ActivityMainBinding
@@ -14,6 +16,8 @@ class MainActivity : AppCompatActivity(), HasSupportFragmentInjector {
     @Inject
     lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Fragment>
 
+    //private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -21,6 +25,10 @@ class MainActivity : AppCompatActivity(), HasSupportFragmentInjector {
             this,
             R.layout.activity_main
         )
+
+        //navController = findNavController(R.id.nav_graph)
+
+        //binding.navHos
     }
 
     override fun supportFragmentInjector() = dispatchingAndroidInjector
--- app/src/main/java/fr/northborders/rickandmorty/ui/CharactersFragment.kt
+++ app/src/main/java/fr/northborders/rickandmorty/ui/CharactersFragment.kt
@@ -1,8 +1,25 @@
 package fr.northborders.rickandmorty.ui
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import fr.northborders.rickandmorty.databinding.FragmentCharactersBinding
 import fr.northborders.rickandmorty.di.Injectable
 
 class CharactersFragment: Fragment(), Injectable {
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = FragmentCharactersBinding.inflate(inflater, container, false)
+
+        (activity as AppCompatActivity).setSupportActionBar(binding.toolbar)
+
+        return binding.root
+    }
 }
