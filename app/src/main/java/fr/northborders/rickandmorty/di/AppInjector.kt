72df1282709f29924a519c156803c9d8f9926d30
Thibault Guegan <thibault@travelloapp.com>
2021-09-06 17:43:35 +1000

    Fix some injection issues

--- app/src/main/java/fr/northborders/rickandmorty/di/AppInjector.kt
+++ app/src/main/java/fr/northborders/rickandmorty/di/AppInjector.kt
@@ -26,27 +26,21 @@ object AppInjector {
             }
 
             override fun onActivityStarted(activity: Activity) {
-                TODO("Not yet implemented")
             }
 
             override fun onActivityResumed(activity: Activity) {
-                TODO("Not yet implemented")
             }
 
             override fun onActivityPaused(activity: Activity) {
-                TODO("Not yet implemented")
             }
 
             override fun onActivityStopped(activity: Activity) {
-                TODO("Not yet implemented")
             }
 
             override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {
-                TODO("Not yet implemented")
             }
 
             override fun onActivityDestroyed(activity: Activity) {
-                TODO("Not yet implemented")
             }
         })
     }
--- app/src/main/java/fr/northborders/rickandmorty/ui/CharactersFragment.kt
+++ app/src/main/java/fr/northborders/rickandmorty/ui/CharactersFragment.kt
@@ -6,19 +6,31 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import fr.northborders.rickandmorty.databinding.FragmentCharactersBinding
 import fr.northborders.rickandmorty.di.Injectable
+import fr.northborders.rickandmorty.di.injectViewModel
+import javax.inject.Inject
 
 class CharactersFragment: Fragment(), Injectable {
+
+    @Inject
+    lateinit var viewModelFactory: ViewModelProvider.Factory
+    lateinit var viewModel: CharactersViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        viewModel = injectViewModel(viewModelFactory)
+
         val binding = FragmentCharactersBinding.inflate(inflater, container, false)
 
         (activity as AppCompatActivity).setSupportActionBar(binding.toolbar)
+
+        val adapter = CharactersAdapter()
+        binding.recyclerView.adapter = adapter
 
         return binding.root
     }
